82c1eb91982625d0762b2140e91daa2954714479
JohnPD5 <jay.pauldu@gmail.com>
2020-10-25 22:38:24 +0100

    logo, icons and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../style/Header.css'
+import logo from '../images/app-logo.jpeg'
 
 const Header = () => {
 	return(
 		<header className="App-header">
+			<img className="logo" src={logo} alt="app-logo"/>
 			<h1 className="title">Cocktail Maker</h1>
 		</header>
 	)
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,13 @@ import '../style/SearchBar.css'
 
 const SearchBar = (props) => {
 	return(
-		<React.Fragment>
-			<button className="back-button" onClick={props.onBack}>Back</button>
-			<form className="search-bar" onSubmit={props.onSearch}>
+		<div className="search-bar">
+			<button className="search-bar__back" onClick={props.onBack}></button>
+			<form className="search-bar__form" onSubmit={props.onSearch}>
 				<input id="search-bar__text" className="search-bar__text" type="text" required/>
-				<input id="search-bar__submit" className="search-bar__submit" type="submit" value="Search"/>
+				<input id="search-bar__submit" className="search-bar__submit" type="submit" value=""/>
 			</form>
-		</React.Fragment>
+		</div>
 	)
 }
 
