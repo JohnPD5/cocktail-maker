e5ffe5f73565e499db45f65d00539a3bd2e94e47
JohnPD5 <jay.pauldu@gmail.com>
2020-10-27 21:34:08 +0100

    fix images lazyload

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -4,7 +4,9 @@ import '../style/SearchItem.css'
 const SearchItem = (props) => {
     return(
         <div id={props.id} className="search-item" onClick={props.getDetail}>
-            <img className="lazy" data-src={props.img} alt={props.name}/>
+            <figure className="search-item__img">
+                <img className="lazy" data-src={props.img} alt={props.name}/>
+            </figure>
             <h3 className="search-item__name">{props.name}</h3>
         </div>
     )
